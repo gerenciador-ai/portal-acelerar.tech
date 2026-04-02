2aa04562f6980bd055b42a1400584fde0841bd16
RCP Servicos <gerenciador@acelerar.tech>
2026-04-01 21:10:00 -0300

    Update TabelasResumo.js

--- src/app/comercial/resultados/TabelasResumo.js
+++ src/app/comercial/resultados/TabelasResumo.js
@@ -80,7 +80,7 @@ export default function TabelasResumo({ tableData }) {
 
     const headersVendas = [
         { key: 'data', label: 'Data', isDate: true },
-        { key: 'CNPJ', label: 'CNPJ', hidden: 'hidden lg:table-cell' }, // CORREÇÃO APLICADA
+        { key: 'CNPJ', label: 'CNPJ' },
         { key: 'cliente', label: 'Cliente' },
         { key: 'vendedor', label: 'Vendedor', hidden: 'hidden md:table-cell' },
         { key: 'sdr', label: 'SDR', hidden: 'hidden lg:table-cell' },
@@ -92,10 +92,10 @@ export default function TabelasResumo({ tableData }) {
 
     const headersCancelamentos = [
         { key: 'data_churn', label: 'Data Churn', isDate: true },
-        { key: 'CNPJ', label: 'CNPJ', hidden: 'hidden lg:table-cell' }, // CORREÇÃO APLICADA
+        { key: 'CNPJ', label: 'CNPJ' },
         { key: 'cliente', label: 'Cliente' },
-        { key: 'vendedor', label: 'Vendedor', hidden: 'hidden md:table-cell' },
-        { key: 'sdr', label: 'SDR', hidden: 'hidden lg:table-cell' },
+        { key: 'Vendedor Original', label: 'Vendedor', hidden: 'hidden md:table-cell' },
+        { key: 'SDR Original', label: 'SDR', hidden: 'hidden lg:table-cell' },
         { key: 'produto', label: 'Produto', hidden: 'hidden md:table-cell' },
         { key: 'mrr', label: 'MRR Perdido', isCurrency: true },
     ];
